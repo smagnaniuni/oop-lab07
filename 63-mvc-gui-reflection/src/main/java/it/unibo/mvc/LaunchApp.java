0cf0b8eb65db5ab6713152638f937d5c72642295
Simone Magnani <simone.magnani7@studio.unibo.it>
2022-11-12 14:14:49 +0100

    Add another graphical view and a console view

--- 63-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
+++ 63-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
@@ -3,6 +3,7 @@ package it.unibo.mvc;
 import it.unibo.mvc.api.DrawNumberController;
 import it.unibo.mvc.controller.DrawNumberControllerImpl;
 import it.unibo.mvc.model.DrawNumberImpl;
+import it.unibo.mvc.view.DrawNumberStdoutView;
 import it.unibo.mvc.view.DrawNumberSwingView;
 
 /**
@@ -27,5 +28,7 @@ public final class LaunchApp {
         final var model = new DrawNumberImpl();
         final DrawNumberController app = new DrawNumberControllerImpl(model);
         app.addView(new DrawNumberSwingView());
+        app.addView(new DrawNumberSwingView());
+        app.addView(new DrawNumberStdoutView());
     }
 }
